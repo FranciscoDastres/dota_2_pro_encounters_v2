a0b4dba8061eb2953701543217ce52720e0328b9
FranciscoDastres <Francisco.meza.dastres@gmail.com>
2026-04-21 11:49:17 -0400

    feat: :zap: Importa y llama startKeepAlive() al arrancar el servidor

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import app from './app'
 import { env } from './config/env'
+import { startKeepAlive } from './keepAlive'
 
 app.listen(env.PORT, () => {
   console.log(`[server] Running on http://localhost:${env.PORT} (${env.NODE_ENV})`)
+  startKeepAlive()
 })
